f138255e58b4b24087d268bbb34b3c21252f0ffa
siavash500 <siavash.yotube1387@example.com>
2025-07-06 20:43:58 -0700

    so i write the getCookie for get the createCooki

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,33 @@
-import React from 'react'
-
+import React , {useEffect}from 'react'
+import { getCookie } from './utilits/helpers/coockis'
 const App =  () => {
-    return <div>App</div>
+    useEffect(()=> {
+        
+    //     const createCooki= async () => {
+    //         await setCookie("credential" , {
+    //             name : "alireza",
+    //             access_token:"awdsad",
+    //             refresh_token: "asdhlui1g2h3"
+    //         });
+    //     };
+
+    //     createCooki();
+
+    // },[]);
+
+         const readCookie= async () => {
+            const result = await getCookie("credential");
+            console.log(result)
+        };
+
+        readCookie();
+    },[]);
+
+
+    return (
+        <>
+            <h1>hello mother fucker</h1>
+        </>
+    )
 }
 export default App;
--- src/utilits/helpers/coockis.js
+++ src/utilits/helpers/coockis.js
@@ -1,2 +1,11 @@
 import Cookies from "js-cookie";
 import {decryptJWT , encryptJWT} from './jwt'
+
+export const setCookie = async (key ,data) =>
+    Cookies.set(key , await encryptJWT(data));
+
+export const getCookie = async(key) =>
+    await decryptJWT(Cookies.get(key));
+
+export const removeCooki = async (key) =>
+    Cookies.remove(key);
